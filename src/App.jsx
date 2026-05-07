a6fd65cc725d9aaee2d9394fecdb8338bf216777
Ni3-Jadhav <ntjadhav2000@gmail.com>
2026-05-07 11:19:22 +0530

    Project setup with login page

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import LoginEmp from "./components/Auth/LoginEmp";
+
+const App = () => {
+  return (
+    
+    <>
+    <LoginEmp />
+    </>
+  )
+};
+
+export default App;
